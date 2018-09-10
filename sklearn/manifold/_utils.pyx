254b5bd832206885e237c48b2a9fe1e8c90e5e68
strategist333 <strategist333>
2018-09-10 09:38:33 -0700

    Playing around with t-SNE ideas

--- sklearn/manifold/_utils.pyx
+++ sklearn/manifold/_utils.pyx
@@ -57,6 +57,7 @@ cpdef np.ndarray[np.float32_t, ndim=2] _binary_search_perplexity(
     cdef float entropy_diff
 
     cdef float entropy
+    cdef float entropy2
     cdef float sum_Pi
     cdef float sum_disti_Pi
     cdef long i, j, k, l
@@ -91,12 +92,17 @@ cpdef np.ndarray[np.float32_t, ndim=2] _binary_search_perplexity(
                 sum_Pi = EPSILON_DBL
             sum_disti_Pi = 0.0
 
+            entropy2 = 0.0
             for j in range(n_neighbors):
                 P[i, j] /= sum_Pi
                 sum_disti_Pi += affinities[i, j] * P[i, j]
+                if P[i, j] > 0.0:
+                    entropy2 -= P[i, j] * math.log(P[i, j])
 
             entropy = math.log(sum_Pi) + beta * sum_disti_Pi
             entropy_diff = entropy - desired_entropy
+            if verbose:
+                print('Beta: %f, alpha: %f, Entropy: %f vs mine: %f; diff: %f, sum_Pi: %f' % (beta, 1 / (math.sqrt(2.0 * beta)), entropy, entropy2, entropy_diff, sum_Pi))
 
             if math.fabs(entropy_diff) <= PERPLEXITY_TOLERANCE:
                 break
@@ -123,4 +129,147 @@ cpdef np.ndarray[np.float32_t, ndim=2] _binary_search_perplexity(
     if verbose:
         print("[t-SNE] Mean sigma: %f"
               % np.mean(math.sqrt(n_samples / beta_sum)))
+    return P
+
+@cython.boundscheck(False)
+cpdef np.ndarray[np.float32_t, ndim=2] _binary_search_perplexity2(
+        np.ndarray[np.float32_t, ndim=3] distances,
+        np.ndarray[np.int32_t, ndim=2] masks,
+        np.ndarray[np.float32_t, ndim=1] col_vars,
+        float desired_perplexity,
+        int verbose):
+    """Binary search for sigmas of conditional Gaussians.
+
+    This approximation reduces the computational complexity from O(N^2) to
+    O(uN). See the exact method '_binary_search_perplexity' for more details.
+
+    Parameters
+    ----------
+    distances : array-like, shape (n_samples, n_samples, dims)
+        Per-dimension distances (Euclidean squared) between training samples
+        and its neighbors (assuming mean imputation)
+
+    masks: array-like, shape (n_samples, dims)
+        0 = observed
+        1 = not observed
+
+    col_vars: array-like, shape (dims,)
+        Empirical standard deviations (squared), for not-observed columns
+
+    desired_perplexity : float
+        Desired perplexity (2^entropy) of the conditional Gaussians.
+
+    verbose : int
+        Verbosity level.
+
+    Returns
+    -------
+    P : array, shape (n_samples, n_samples)
+        Probabilities of conditional Gaussian distributions p_i|j.
+    """
+    # Maximum number of binary search steps
+    cdef long n_steps = 100
+
+    cdef long n_samples = distances.shape[0]
+    cdef long n_dims = distances.shape[2]
+    # Precisions of conditional Gaussian distributions
+    cdef float alpha
+    cdef float alpha_sq
+    cdef float alpha_min
+    cdef float alpha_max
+    cdef float log_alpha
+    cdef float variance
+
+    # Use log scale
+    cdef float desired_entropy = math.log(desired_perplexity)
+    cdef float entropy_diff
+
+    cdef float entropy
+    cdef float sum_Pi
+    cdef float log_sum_Pi
+    cdef float max_Q
+    cdef long i, j, k, l
+
+    # This array is later used as a 32bit array. It has multiple intermediate
+    # floating point additions that benefit from the extra precision
+    cdef np.ndarray[np.float64_t, ndim=2] P = np.zeros(
+        (n_samples, n_samples), dtype=np.float64)
+    # Q = log(P)
+    cdef np.ndarray[np.float64_t, ndim=2] Q = np.zeros(
+        (n_samples, n_samples), dtype=np.float64)
+
+    for i in range(n_samples):
+        alpha_min = 0.0
+        alpha_max = NPY_INFINITY
+        alpha = math.sqrt(0.5)
+
+        # Binary search of precision for i-th conditional distribution
+        for l in range(n_steps):
+            alpha_sq = alpha * alpha
+            log_alpha = math.log(alpha)
+            # Compute current entropy and corresponding probabilities
+            # computed just over the nearest neighbors or over all data
+            # if we're not using neighbors
+            sum_Pi = 0.0
+            l = -1
+            for j in range(n_samples):
+                if j != i:
+                    Q[i, j] = 0.0
+                    for k in range(n_dims):
+                        if masks[i, k] == 0:
+                            if masks[j, k] == 0:
+                                variance = alpha_sq
+                                Q[i, j] += (-distances[i, j, k] / (2.0 * variance)) - log_alpha
+                            else:
+                                variance = alpha_sq + col_vars[k]
+                                Q[i, j] += (-distances[i, j, k] / (2.0 * variance)) - math.log(variance) * 0.5
+                        else:
+                            if masks[j, k] == 0:
+                                variance = alpha_sq + col_vars[k]
+                                Q[i, j] += (-distances[i, j, k] / (2.0 * variance)) - math.log(variance) * 0.5
+                            else:
+                                variance = alpha_sq + 2 * col_vars[k]
+                                Q[i, j] += -math.log(variance) * 0.5
+                    if l == -1 or (Q[i, j] > Q[i, l]):
+                        l = j
+            max_Q = Q[i, l]
+            #print('%d %f' % (l, max_Q))
+            for j in range(n_samples):
+                if j != i:
+                    Q[i, j] -= max_Q
+                    P[i, j] = math.exp(Q[i, j])
+                    #print('%d %d %f %f %f' % (i, j, Q[i, j], P[i, j], sum_Pi))
+                    sum_Pi += P[i, j]
+
+            if sum_Pi == 0.0:
+                sum_Pi = EPSILON_DBL
+            entropy = 0.0
+
+            log_sum_Pi = math.log(sum_Pi)
+            for j in range(n_samples):
+                if j != i:
+                    P[i, j] /= sum_Pi
+                    if P[i, j] > 0.0:
+                        entropy -= P[i, j] * (Q[i, j] - log_sum_Pi)
+            entropy_diff = entropy - desired_entropy
+            if verbose:
+                print('Alpha: %f, Entropy: %f, diff: %f, sum_Pi: %f' % (alpha, entropy, entropy_diff, sum_Pi))
+
+            if math.fabs(entropy_diff) <= PERPLEXITY_TOLERANCE:
+                break
+
+            if entropy_diff > 0.0:
+                alpha_max = alpha
+                alpha = math.sqrt((alpha * alpha + alpha_min * alpha_min) / 2.0)
+            else:
+                alpha_min = alpha
+                if alpha_max == NPY_INFINITY:
+                    alpha *= math.sqrt(2.0)
+                else:
+                    alpha = math.sqrt((alpha * alpha + alpha_max * alpha_max) / 2.0)
+
+        if verbose and ((i + 1) % 1000 == 0 or i + 1 == n_samples):
+            print("[t-SNE] Computed conditional probabilities for sample "
+                  "%d / %d" % (i + 1, n_samples))
+
     return P
--- sklearn/manifold/t_sne.py
+++ sklearn/manifold/t_sne.py
@@ -64,6 +64,37 @@ def _joint_probabilities(distances, desired_perplexity, verbose):
     return P
 
 
+def _joint_probabilities2(distances, masks, col_vars, desired_perplexity, verbose):
+    """Compute joint probabilities p_ij from distances.
+
+    Parameters
+    ----------
+    distances : array, shape (n_samples * (n_samples-1) / 2,)
+        Distances of samples are stored as condensed matrices, i.e.
+        we omit the diagonal and duplicate entries and store everything
+        in a one-dimensional array.
+
+    desired_perplexity : float
+        Desired perplexity of the joint probability distributions.
+
+    verbose : int
+        Verbosity level.
+
+    Returns
+    -------
+    P : array, shape (n_samples * (n_samples-1) / 2,)
+        Condensed joint probability matrix.
+    """
+    # Compute conditional probabilities such that they approximately match
+    # the desired perplexity
+    distances = distances.astype(np.float32, copy=False)
+    conditional_P = _utils._binary_search_perplexity2(
+        distances, masks.astype(np.int32), col_vars, desired_perplexity, verbose)
+    P = conditional_P + conditional_P.T
+    sum_P = np.maximum(np.sum(P), MACHINE_EPSILON)
+    P = np.maximum(squareform(P) / sum_P, MACHINE_EPSILON)
+    return P
+
 def _joint_probabilities_nn(distances, neighbors, desired_perplexity, verbose):
     """Compute joint probabilities p_ij from distances using just nearest
     neighbors.
@@ -910,3 +941,397 @@ class TSNE(BaseEstimator):
         """
         self.fit_transform(X)
         return self
+
+
+
+
+
+
+
+
+
+
+
+
+class TSNE2(BaseEstimator):
+    """t-distributed Stochastic Neighbor Embedding.
+
+    t-SNE [1] is a tool to visualize high-dimensional data. It converts
+    similarities between data points to joint probabilities and tries
+    to minimize the Kullback-Leibler divergence between the joint
+    probabilities of the low-dimensional embedding and the
+    high-dimensional data. t-SNE has a cost function that is not convex,
+    i.e. with different initializations we can get different results.
+
+    It is highly recommended to use another dimensionality reduction
+    method (e.g. PCA for dense data or TruncatedSVD for sparse data)
+    to reduce the number of dimensions to a reasonable amount (e.g. 50)
+    if the number of features is very high. This will suppress some
+    noise and speed up the computation of pairwise distances between
+    samples. For more tips see Laurens van der Maaten's FAQ [2].
+
+    Read more in the :ref:`User Guide <t_sne>`.
+
+    Parameters
+    ----------
+    n_components : int, optional (default: 2)
+        Dimension of the embedded space.
+
+    perplexity : float, optional (default: 30)
+        The perplexity is related to the number of nearest neighbors that
+        is used in other manifold learning algorithms. Larger datasets
+        usually require a larger perplexity. Consider selecting a value
+        between 5 and 50. The choice is not extremely critical since t-SNE
+        is quite insensitive to this parameter.
+
+    early_exaggeration : float, optional (default: 12.0)
+        Controls how tight natural clusters in the original space are in
+        the embedded space and how much space will be between them. For
+        larger values, the space between natural clusters will be larger
+        in the embedded space. Again, the choice of this parameter is not
+        very critical. If the cost function increases during initial
+        optimization, the early exaggeration factor or the learning rate
+        might be too high.
+
+    learning_rate : float, optional (default: 200.0)
+        The learning rate for t-SNE is usually in the range [10.0, 1000.0]. If
+        the learning rate is too high, the data may look like a 'ball' with any
+        point approximately equidistant from its nearest neighbours. If the
+        learning rate is too low, most points may look compressed in a dense
+        cloud with few outliers. If the cost function gets stuck in a bad local
+        minimum increasing the learning rate may help.
+
+    n_iter : int, optional (default: 1000)
+        Maximum number of iterations for the optimization. Should be at
+        least 250.
+
+    n_iter_without_progress : int, optional (default: 300)
+        Maximum number of iterations without progress before we abort the
+        optimization, used after 250 initial iterations with early
+        exaggeration. Note that progress is only checked every 50 iterations so
+        this value is rounded to the next multiple of 50.
+
+        .. versionadded:: 0.17
+           parameter *n_iter_without_progress* to control stopping criteria.
+
+    min_grad_norm : float, optional (default: 1e-7)
+        If the gradient norm is below this threshold, the optimization will
+        be stopped.
+
+    metric : string or callable, optional
+        The metric to use when calculating distance between instances in a
+        feature array. If metric is a string, it must be one of the options
+        allowed by scipy.spatial.distance.pdist for its metric parameter, or
+        a metric listed in pairwise.PAIRWISE_DISTANCE_FUNCTIONS.
+        If metric is "precomputed", X is assumed to be a distance matrix.
+        Alternatively, if metric is a callable function, it is called on each
+        pair of instances (rows) and the resulting value recorded. The callable
+        should take two arrays from X as input and return a value indicating
+        the distance between them. The default is "euclidean" which is
+        interpreted as squared euclidean distance.
+
+    init : string or numpy array, optional (default: "random")
+        Initialization of embedding. Possible options are 'random', 'pca',
+        and a numpy array of shape (n_samples, n_components).
+        PCA initialization cannot be used with precomputed distances and is
+        usually more globally stable than random initialization.
+
+    verbose : int, optional (default: 0)
+        Verbosity level.
+
+    random_state : int, RandomState instance or None, optional (default: None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.  Note that different initializations might result in
+        different local minima of the cost function.
+
+    method : string (default: 'barnes_hut')
+        By default the gradient calculation algorithm uses Barnes-Hut
+        approximation running in O(NlogN) time. method='exact'
+        will run on the slower, but exact, algorithm in O(N^2) time. The
+        exact algorithm should be used when nearest-neighbor errors need
+        to be better than 3%. However, the exact method cannot scale to
+        millions of examples.
+
+        .. versionadded:: 0.17
+           Approximate optimization *method* via the Barnes-Hut.
+
+    angle : float (default: 0.5)
+        Only used if method='barnes_hut'
+        This is the trade-off between speed and accuracy for Barnes-Hut T-SNE.
+        'angle' is the angular size (referred to as theta in [3]) of a distant
+        node as measured from a point. If this size is below 'angle' then it is
+        used as a summary node of all points contained within it.
+        This method is not very sensitive to changes in this parameter
+        in the range of 0.2 - 0.8. Angle less than 0.2 has quickly increasing
+        computation time and angle greater 0.8 has quickly increasing error.
+
+    Attributes
+    ----------
+    embedding_ : array-like, shape (n_samples, n_components)
+        Stores the embedding vectors.
+
+    kl_divergence_ : float
+        Kullback-Leibler divergence after optimization.
+
+    n_iter_ : int
+        Number of iterations run.
+
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from sklearn.manifold import TSNE
+    >>> X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
+    >>> X_embedded = TSNE(n_components=2).fit_transform(X)
+    >>> X_embedded.shape
+    (4, 2)
+
+    References
+    ----------
+
+    [1] van der Maaten, L.J.P.; Hinton, G.E. Visualizing High-Dimensional Data
+        Using t-SNE. Journal of Machine Learning Research 9:2579-2605, 2008.
+
+    [2] van der Maaten, L.J.P. t-Distributed Stochastic Neighbor Embedding
+        https://lvdmaaten.github.io/tsne/
+
+    [3] L.J.P. van der Maaten. Accelerating t-SNE using Tree-Based Algorithms.
+        Journal of Machine Learning Research 15(Oct):3221-3245, 2014.
+        http://lvdmaaten.github.io/publications/papers/JMLR_2014.pdf
+    """
+    # Control the number of exploration iterations with early_exaggeration on
+    _EXPLORATION_N_ITER = 250
+
+    # Control the number of iterations between progress checks
+    _N_ITER_CHECK = 50
+
+    def __init__(self, n_components=2, perplexity=30.0,
+                 early_exaggeration=12.0, learning_rate=200.0, n_iter=1000,
+                 n_iter_without_progress=300, min_grad_norm=1e-7,
+                 metric="euclidean", init="random", verbose=0,
+                 random_state=None, method='barnes_hut', angle=0.5):
+        self.n_components = n_components
+        self.perplexity = perplexity
+        self.early_exaggeration = early_exaggeration
+        self.learning_rate = learning_rate
+        self.n_iter = n_iter
+        self.n_iter_without_progress = n_iter_without_progress
+        self.min_grad_norm = min_grad_norm
+        self.metric = metric
+        self.init = init
+        self.verbose = verbose
+        self.random_state = random_state
+        self.method = method
+        self.angle = angle
+
+    def _fit(self, X, masks, col_vars, skip_num_points=0):
+        """Fit the model using X as training data.
+
+        Note that sparse arrays can only be handled by method='exact'.
+        It is recommended that you convert your sparse array to dense
+        (e.g. `X.toarray()`) if it fits in memory, or otherwise using a
+        dimensionality reduction technique (e.g. TruncatedSVD).
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features) or (n_samples, n_samples)
+            If the metric is 'precomputed' X must be a square distance
+            matrix. Otherwise it contains a sample per row. Note that this
+            when method='barnes_hut', X cannot be a sparse array and if need be
+            will be converted to a 32 bit float array. Method='exact' allows
+            sparse arrays and 64bit floating point inputs.
+
+        skip_num_points : int (optional, default:0)
+            This does not compute the gradient for points with indices below
+            `skip_num_points`. This is useful when computing transforms of new
+            data where you'd like to keep the old data fixed.
+        """
+        if self.method not in ['barnes_hut', 'exact']:
+            raise ValueError("'method' must be 'barnes_hut' or 'exact'")
+        if self.angle < 0.0 or self.angle > 1.0:
+            raise ValueError("'angle' must be between 0.0 - 1.0")
+        if self.metric == "precomputed":
+            if isinstance(self.init, string_types) and self.init == 'pca':
+                raise ValueError("The parameter init=\"pca\" cannot be "
+                                 "used with metric=\"precomputed\".")
+            if X.shape[0] != X.shape[1]:
+                raise ValueError("X should be a square distance matrix")
+            if np.any(X < 0):
+                raise ValueError("All distances should be positive, the "
+                                 "precomputed distances given as X is not "
+                                 "correct")
+        if self.method == 'barnes_hut' and sp.issparse(X):
+            raise TypeError('A sparse matrix was passed, but dense '
+                            'data is required for method="barnes_hut". Use '
+                            'X.toarray() to convert to a dense numpy array if '
+                            'the array is small enough for it to fit in '
+                            'memory. Otherwise consider dimensionality '
+                            'reduction techniques (e.g. TruncatedSVD)')
+        if self.method == 'barnes_hut':
+            X = check_array(X, ensure_min_samples=2,
+                            dtype=[np.float32, np.float64])
+        else:
+            X = check_array(X, accept_sparse=['csr', 'csc', 'coo'],
+                            ensure_2d=False, allow_nd=True,
+                            dtype=[np.float32, np.float64])
+        if self.method == 'barnes_hut' and self.n_components > 3:
+            raise ValueError("'n_components' should be inferior to 4 for the "
+                             "barnes_hut algorithm as it relies on "
+                             "quad-tree or oct-tree.")
+        random_state = check_random_state(self.random_state)
+
+        if self.early_exaggeration < 1.0:
+            raise ValueError("early_exaggeration must be at least 1, but is {}"
+                             .format(self.early_exaggeration))
+
+        if self.n_iter < 250:
+            raise ValueError("n_iter should be at least 250")
+
+        n_samples = X.shape[0]
+
+        neighbors_nn = None
+        assert self.method == "exact"
+        # Retrieve the distance matrix, either using the precomputed one or
+        # computing it.
+        assert self.metric == "precomputed"
+        distances = X
+
+        # compute the joint probability distribution for the input space
+        P = _joint_probabilities2(distances, masks, col_vars, self.perplexity, self.verbose)
+        assert np.all(np.isfinite(P)), "All probabilities should be finite"
+        assert np.all(P >= 0), "All probabilities should be non-negative"
+        assert np.all(P <= 1), ("All probabilities should be less "
+                                "or then equal to one")
+
+        if isinstance(self.init, np.ndarray):
+            X_embedded = self.init
+        elif self.init == 'pca':
+            pca = PCA(n_components=self.n_components, svd_solver='randomized',
+                      random_state=random_state)
+            X_embedded = pca.fit_transform(X).astype(np.float32, copy=False)
+        elif self.init == 'random':
+            # The embedding is initialized with iid samples from Gaussians with
+            # standard deviation 1e-4.
+            X_embedded = 1e-4 * random_state.randn(
+                n_samples, self.n_components).astype(np.float32)
+        else:
+            raise ValueError("'init' must be 'pca', 'random', or "
+                             "a numpy array")
+
+        # Degrees of freedom of the Student's t-distribution. The suggestion
+        # degrees_of_freedom = n_components - 1 comes from
+        # "Learning a Parametric Embedding by Preserving Local Structure"
+        # Laurens van der Maaten, 2009.
+        degrees_of_freedom = max(self.n_components - 1, 1)
+
+        return self._tsne(P, degrees_of_freedom, n_samples,
+                          X_embedded=X_embedded,
+                          neighbors=neighbors_nn,
+                          skip_num_points=skip_num_points)
+
+    @property
+    @deprecated("Attribute n_iter_final was deprecated in version 0.19 and "
+                "will be removed in 0.21. Use ``n_iter_`` instead")
+    def n_iter_final(self):
+        return self.n_iter_
+
+    def _tsne(self, P, degrees_of_freedom, n_samples, X_embedded,
+              neighbors=None, skip_num_points=0):
+        """Runs t-SNE."""
+        # t-SNE minimizes the Kullback-Leiber divergence of the Gaussians P
+        # and the Student's t-distributions Q. The optimization algorithm that
+        # we use is batch gradient descent with two stages:
+        # * initial optimization with early exaggeration and momentum at 0.5
+        # * final optimization with momentum at 0.8
+        params = X_embedded.ravel()
+
+        opt_args = {
+            "it": 0,
+            "n_iter_check": self._N_ITER_CHECK,
+            "min_grad_norm": self.min_grad_norm,
+            "learning_rate": self.learning_rate,
+            "verbose": self.verbose,
+            "kwargs": dict(skip_num_points=skip_num_points),
+            "args": [P, degrees_of_freedom, n_samples, self.n_components],
+            "n_iter_without_progress": self._EXPLORATION_N_ITER,
+            "n_iter": self._EXPLORATION_N_ITER,
+            "momentum": 0.5,
+        }
+        if self.method == 'barnes_hut':
+            obj_func = _kl_divergence_bh
+            opt_args['kwargs']['angle'] = self.angle
+            # Repeat verbose argument for _kl_divergence_bh
+            opt_args['kwargs']['verbose'] = self.verbose
+        else:
+            obj_func = _kl_divergence
+
+        # Learning schedule (part 1): do 250 iteration with lower momentum but
+        # higher learning rate controlled via the early exageration parameter
+        P *= self.early_exaggeration
+        params, kl_divergence, it = _gradient_descent(obj_func, params,
+                                                      **opt_args)
+        if self.verbose:
+            print("[t-SNE] KL divergence after %d iterations with early "
+                  "exaggeration: %f" % (it + 1, kl_divergence))
+
+        # Learning schedule (part 2): disable early exaggeration and finish
+        # optimization with a higher momentum at 0.8
+        P /= self.early_exaggeration
+        remaining = self.n_iter - self._EXPLORATION_N_ITER
+        if it < self._EXPLORATION_N_ITER or remaining > 0:
+            opt_args['n_iter'] = self.n_iter
+            opt_args['it'] = it + 1
+            opt_args['momentum'] = 0.8
+            opt_args['n_iter_without_progress'] = self.n_iter_without_progress
+            params, kl_divergence, it = _gradient_descent(obj_func, params,
+                                                          **opt_args)
+
+        # Save the final number of iterations
+        self.n_iter_ = it
+
+        if self.verbose:
+            print("[t-SNE] KL divergence after %d iterations: %f"
+                  % (it + 1, kl_divergence))
+
+        X_embedded = params.reshape(n_samples, self.n_components)
+        self.kl_divergence_ = kl_divergence
+
+        return X_embedded
+
+    def fit_transform(self, distances, masks, col_vars):
+        """Fit X into an embedded space and return that transformed
+        output.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features) or (n_samples, n_samples)
+            If the metric is 'precomputed' X must be a square distance
+            matrix. Otherwise it contains a sample per row.
+
+        y : Ignored
+
+        Returns
+        -------
+        X_new : array, shape (n_samples, n_components)
+            Embedding of the training data in low-dimensional space.
+        """
+        embedding = self._fit(distances, masks, col_vars)
+        self.embedding_ = embedding
+        return self.embedding_
+
+    def fit(self, X, y=None):
+        """Fit X into an embedded space.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features) or (n_samples, n_samples)
+            If the metric is 'precomputed' X must be a square distance
+            matrix. Otherwise it contains a sample per row. If the method
+            is 'exact', X may be a sparse matrix of type 'csr', 'csc'
+            or 'coo'.
+
+        y : Ignored
+        """
+        raise NotImplementedError
